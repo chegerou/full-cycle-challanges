0f12f798ea1fc14050af27b43b1e591605d13536
Igor Afonso Nunes <igorafonsonunes@outlook.com>
2022-02-10 21:58:39 -0300

    Entrega do segundo desafio docker, utilizando o nginx como proxy reverso para o nodejs.

---
+++ docker_modulo/node/service/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+var mysql = require('mysql');
+const app = express();
+const port = 3000;
+var config ={
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+
+const connection = mysql.createConnection(config);
+
+app.get('/', (req, res)=>{
+    
+    connection.query('INSERT INTO people(name) Values("Aluno '+Math.floor(Math.random()*100)+'")');
+
+    connection.query('SELECT id, name FROM people', function(error, results, fields){
+        var html = `
+                    <style>
+                    table, th, td {
+                    border:1px solid black;
+                    }
+                    </style>
+                    <h1>Full Cycle Rocks!</h1>
+                    <table>
+                        <tr>
+                            <th>Nome Do Aluno</th>
+                        </tr>
+        `
+        results.map(nome => {
+            html = html + '<tr><td>'+nome.name+'</td></tr>'
+        });
+        
+        html = html + ' </table>'
+        res.send(html);
+    });
+    console.log("Chamada")
+});
+
+app.listen(port);
+console.log('Executando na porta: ' +port);
