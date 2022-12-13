45ef26877d0f57a99746c2b7e0c8913618aab761
Igor Afonso Nunes <igor.nunes@solarview.com.br>
2022-12-13 19:21:16 -0300

    Criado customer functions

--- ddd_modulo/src/domain/entity/address.ts
+++ ddd_modulo/src/domain/entity/address.ts
@@ -1,36 +1,52 @@
 //Objeto de Valor - Não pode ser alterado.
-export default class Address{
-   private __street: string = "";
-   private __number: number = 0;
-   private __zip: string = "";
-   private __city: string = "";
-   private __active: boolean = false;
+export default class Address {
+    private __street: string = "";
+    private __number: number = 0;
+    private __zip: string = "";
+    private __city: string = "";
+    private __active: boolean = false;
 
-   constructor(street: string,number: number, zip: string, city: string){
-     this.__street = street;
-     this.__number = number;
-     this.__zip = zip;
-     this.__city = city;
+    constructor(street: string, number: number, zip: string, city: string) {
+        this.__street = street;
+        this.__number = number;
+        this.__zip = zip;
+        this.__city = city;
 
-     this.validate();
-   }
-
-   validate(): boolean{
-    
-    if(this.__street.length === 0){
-        throw new Error("street is required.")
-    }
-    if(this.__number <= 0){
-        throw new Error("number is required.")
-    }
-    if(this.__zip.length === 0){
-        throw new Error("zipcode is required.")
-    }
-    if(this.__city.length === 0){
-        throw new Error("city is required.")
+        this.validate();
     }
 
-    return true;
-   }
+    get street() {
+        return this.__street;
+    }
+
+    get number() {
+        return this.__number;
+    }
+
+    get zip() {
+        return this.__zip;
+    }
+
+    get city() {
+        return this.__city;
+    }
+
+    validate(): boolean {
+
+        if (this.__street.length === 0) {
+            throw new Error("street is required.")
+        }
+        if (this.__number <= 0) {
+            throw new Error("number is required.")
+        }
+        if (this.__zip.length === 0) {
+            throw new Error("zipcode is required.")
+        }
+        if (this.__city.length === 0) {
+            throw new Error("city is required.")
+        }
+
+        return true;
+    }
 
 }
