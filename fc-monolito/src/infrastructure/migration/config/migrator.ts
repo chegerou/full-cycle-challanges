96d979d930500625082885df5ccd3f5c3374da49
Igor Afonso Nunes <igor.nunes@estrelabet.com>
2024-05-25 23:41:54 -0300

    fix: adding a new way to migrations.

--- fc-monolito/src/infrastructure/migration/config/migrator.ts
+++ fc-monolito/src/infrastructure/migration/config/migrator.ts
@@ -4,7 +4,7 @@ import { Umzug, SequelizeStorage } from "umzug";
 export const migrator = (sequelize: Sequelize) => {
   return new Umzug({
     migrations: {
-      glob: '',
+      glob: 'src/infrastructure/migration/migrations/*.ts',
     },
     storage: new SequelizeStorage({ sequelize }),
     context: sequelize.getQueryInterface(),
