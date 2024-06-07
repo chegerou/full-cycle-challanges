2f55d32bae70d0518c52cae0b428ea2c21b474ce
Igor Afonso Nunes <igor.nunes@estrelabet.com>
2024-06-07 11:29:47 -0300

    chore: removing the comment.

--- fc-monolito/src/infrastructure/api/server.ts
+++ fc-monolito/src/infrastructure/api/server.ts
@@ -3,7 +3,6 @@ import Database from "./config/database";
 import App from "./app";
 import Routes from "./routes";
 
-//Start the server
 const routes = new Routes();
 const database = new Database();
 const app = new App(routes, database);
